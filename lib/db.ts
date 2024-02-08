93603ee533c12014c093cd9c36d9a8856d61d7e4
Eduardo V. Micami <eduvnsm@gmail.com>
2024-02-08 15:46:19 -0300

    Setted up Database and Prisma

---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+/**
+ * For production enviroment you just need the code below
+ */
+// export const db = new PrismaClient();
